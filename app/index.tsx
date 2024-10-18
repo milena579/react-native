51e77cb2c3251f52919812ee92bcaff33feaf101
milena579 <milecalegari@gmail.com>
2024-10-18 17:13:46 -0300

    começando tela de login

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { Link, router} from "expo-router";
-import { useState } from "react";
-import { View, Text, TextInput, SafeAreaView, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { LinearGradient } from 'expo-linear-gradient';
+import { View, Text, TextInput, SafeAreaView, StyleSheet, TouchableOpacity, ImageBackground } from "react-native";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -15,57 +16,103 @@ export default function Login() {
 
   console.log(email, pass);
   console.log(typeof email, typeof pass);
+
+  const image = {uri:'https://w0.peakpx.com/wallpaper/1010/973/HD-wallpaper-minimal-landscape-design-draw-landscape-minimal-mountain-nature-sky-tree-trending-trendy.jpg'}
   return (
     <>
-      <SafeAreaView>
-        <Text>Login</Text>
-        <Text>E-mail</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={setEmail}
-          value={email}
-          keyboardType="email-address"
-          placeholder="Digite seu email"
-        />
-        <Text>Senha</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={setPass}
-          value={pass}
-          placeholder="Ensira sua senha"
-          keyboardType="default"
-          secureTextEntry={true}
-        />
-      </SafeAreaView>
+      <View style={styles.tela}>
+        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+          <Text style={styles.titulo}>Login</Text>
+          <View style={styles.login}>
+            <Text style={styles.label}>E-mail</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={setEmail}
+              value={email}
+              keyboardType="email-address"
+              placeholder="Digite seu email"
+            />
+            <Text style={styles.label}>Senha</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={setPass}
+              value={pass}
+              placeholder="Ensira sua senha"
+              keyboardType="default"
+              secureTextEntry={true}
+            />
+            <View>
+              {/* <View>
+                <Text>Count: {count}</Text>
+              </View> */}
+              <LinearGradient colors={['#614A97FF', '#6950C0FF', '#2A213AFF']} style={styles.button}>
+                <TouchableOpacity style={styles.button} onPress={onPress}>
+                  <Text style={styles.btnText}>Entrar</Text>
+                </TouchableOpacity>
+              </LinearGradient>
 
-      <View>
-        {/* <View>
-          <Text>Count: {count}</Text>
-        </View> */}
-        <TouchableOpacity style={styles.button} onPress={onPress}>
-          <Text style={styles.btnText}>Entrar</Text>
-        </TouchableOpacity>
-        <View>
-            <Link href={"register"}>Cadastrar novo usuário</Link>
-        </View>
+              <View>
+                  <Link href={"register"}>Cadastrar novo usuário</Link>
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  tela : {
+    display: "flex",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  login : {
+    display:"flex",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    gap:15,
+    backgroundColor: "#ffffff",
+    height:"50%",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius:15
+  },
+  titulo : {
+    display: "flex",
+    width: "100%",
+    justifyContent: "center",
+    paddingVertical: 20,
+    fontSize: 25,
+    fontWeight: "bold"
+  },
+  label :{
+    display:"flex",
+    width: "100%"
+  },
   input: {
     paddingHorizontal: 10,
-    paddingVertical: 3,
+    paddingVertical: 8,
     backgroundColor: "#505050FF",
     color: "#ffffff",
+    width: "100%",
+    borderRadius: 10
   },
   button : {
     paddingHorizontal: 5,
     paddingVertical : 5,
-    backgroundColor: "#1C7996FF"
+    borderRadius:15,
+    alignItems: "center"
   },
   btnText : {
     color: "#ffffff"
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+    justifyContent: 'center',
   }
 });
